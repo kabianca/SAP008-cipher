71ad3704e6ee604f3f09dca692ff46e4d83263f5
Bianca Oliveira <bianca@violet>
2022-07-21 10:53:45 -0300

    encode e decode prontos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const encodeBtn = document.getElementById('btnEncode');
+const decodeBtn = document.getElementById('btnDecode');
+
+encodeBtn.addEventListener('click', () => {
+  let original = document.getElementById('code').value.toUpperCase();
+  let shift = parseInt(document.getElementById('offsetCode').value);
+  let result = cipher.encode(original, shift);
+  document.getElementById("codeMsg").value = result;
+});
+
+decodeBtn.addEventListener('click', () => {
+  let encryptMsg = document.getElementById('code').value.toUpperCase();
+  let shiftDecode = parseInt(document.getElementById('offsetCode').value);
+  let resultdecode = cipher.decode(encryptMsg, shiftDecode);
+  document.getElementById("codeMsg").value = resultdecode;
+});
